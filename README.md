99a8db81c437b3c173bf29e6eafa1d9b68fccf4c
Andras <andras@kinvey.com>
2018-02-21 15:06:27 -0500

    simplify configs, auto-start only if run directly, validate options, update readme

--- README.md
+++ README.md
@@ -1,10 +1,44 @@
-kinvey-prom
-===========
+fireman-pushgateway
+===================
 
-Kinvey Prometheus agent.
+Embeddable aggregating Prometheus push gateway.
+
+Accepts stats from an app and presents them to be scraped by Prometheus.  Same-named
+stats are averaged, which allows stats to be gathered more frequently than the scrape
+interval, and simplifies stats reporting from multi-threaded apps.
+
+All metrics are treated as untyped values, ie gauges.
 
 
-TODO
+Overview
+--------
+
+The gateway forks itself into a separate process and listens for requests.
+
+
+Api
+---
+
+
+Http Api
+--------
+
+The gateway listens on the configured port (default 9091) for http requests.
+
+### /push
+
+Push stats to prometheus.
+
+### /metrics
+
+Endpoint used by prometheus to scrape 
+
+### /healthcheck
+
+Returns 200 OK status code and "OK" body.
+
+
+Todo
 ----
 
 - checkpoint metrics into a local journal (to back up the in-memory copy).
--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 'use strict';
 
-const pkg = require('./package');
-const config = require('config');
-const Gateway = require('./lib/gateway');
+const path = require('path');
 
-const qerror = require('qerror');
-qerror.handler = (err, callback) => {
-    callback();
+module.exports = {
+    createServer: function createServer( config, callback ) {
+        const journalFilename = config.journalFilename || config.logDir && config.journalName && (config.logDir + '/' + config.journalName) || null;
+        const options = {
+            port: config.port || 9091,
+            journalFilename: journalFilename,
+            labels: config.labels || {},
+        };
+        require('./lib/app').createServer(options, (err, info) => {
+            callback(err, info);
+        })
+    },
+};
+
+
+// if invoked standalone, start the service
+const sourceFilename = require.resolve(__filename);
+if (sourceFilename === process.argv[1] || path.dirname(sourceFilename) === process.argv[1]) {
+    const pkg = require('./package');
+    const config = require('config');
+
+    const Gateway = require('./lib/gateway');
+    Gateway.trace('%s: Starting.', pkg.name);
+
+    const qerror = require('qerror');
+    qerror.handler = (err, callback) => {
+        Gateway.trace('%s: Exiting on %s', pkg.name, err);
+        callback();
+    }
+
+    module.exports.createServer(config, (err, info) => {
+        Gateway.trace('%s: Listening on %d.', pkg.name, info.port);
+        process.kill(0, 'SIGHUP');
+    })
 }
-
-require('./lib/app').createServer(config, (err, info) => {
-    Gateway.trace('%s: Listening on %d', pkg.name, config.server.port);
-})
