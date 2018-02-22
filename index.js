5fd14ded7b1cc0d3060aacfa7c8a62a23a6d5eca
Andras <andras@kinvey.com>
2018-02-22 14:25:37 -0500

    refactor index.js into a pushGateway singleton

--- index.js
+++ index.js
@@ -3,38 +3,42 @@
 const path = require('path');
 const child_process = require('child_process');
 
-const serviceScriptFilename = __dirname + '/lib/service.js';
-
-module.exports = {
-    createServer: function createServer( config, callback ) {
-        const options = buildServerOptions(config);
-        require('./lib/app').createServer(options, (err, info) => {
-            callback(err, info);
-        })
-    },
-
-    forkServer: function forkServer( config, callback ) {
-        const options = buildServerOptions(config);
-        var worker;
-        try {
-            worker = child_process.fork(serviceScriptFilename);
-            worker.send({ n: 'createServer', m: options });
-        } catch (err) {
-            return callback(err);
-        }
-        worker.on('message', (msg) => {
-            if (!msg || !msg.n) return;
-            if (msg.n === 'error') callback(msg.m);
-            if (msg.n === 'ready') callback(null, msg.m);
-        })
-        return worker;
-    },
-};
+const pushGateway = new PushGateway();
+module.exports = pushGateway;
 
 
-// if invoked standalone, start the service
-const sourceFilename = require.resolve(__filename);
-if (sourceFilename === process.argv[1] || path.dirname(sourceFilename) === process.argv[1]) {
+function PushGateway( ) {
+}
+
+PushGateway.prototype.createServer = function createServer( config, callback ) {
+    const options = this._buildServerOptions(config);
+    require('./lib/app').createServer(options, (err, info) => {
+        callback(err, info);
+    })
+}
+
+PushGateway.prototype.forkServer = function forkServer( config, callback ) {
+    const options = this._buildServerOptions(config);
+    var worker;
+
+    try {
+        worker = child_process.fork(__dirname + '/lib/service.js');
+        worker.send({ n: 'createServer', m: options });
+    } catch (err) {
+        return callback(err);
+    }
+
+    worker.on('message', (msg) => {
+        if (!msg || !msg.n) return;
+        if (msg.n === 'error') callback(msg.m);
+        if (msg.n === 'ready') callback(null, msg.m);
+    })
+
+    return worker;
+}
+
+// run this process as a standalone server
+PushGateway.prototype.runServer = function runServer( callback ) {
     const pkg = require('./package');
     const config = require('config');
 
@@ -44,31 +48,36 @@ if (sourceFilename === process.argv[1] || path.dirname(sourceFilename) === proce
     const qerror = require('qerror');
     qerror.alert = false;
     qerror.handler = (err, callback) => {
-        Gateway.trace('%s: Exiting on %s', pkg.name, err);
+        const stacktrace = err && !/^SIG/.test(err.message) && err.stack || '';
+        Gateway.trace('%s: Exiting on %s', pkg.name, err, "\n", process.memoryUsage(), stacktrace);
         callback();
     }
 
-    module.exports.createServer(config, (err, info) => {
+    this.createServer(config, (err, info) => {
         Gateway.trace('%s: Listening on %d.', pkg.name, info.port);
-        process.kill(0, 'SIGHUP');
+        if (callback) callback(err, info);
     })
 }
 
-function buildServerOptions( config ) {
-    const journalFilename = config.journalFilename || config.logDir && config.journalName && (config.logDir + '/' + config.journalName) || null;
+PushGateway.prototype._buildServerOptions = function _buildServerOptions( config ) {
     var options = {
         port: config.port || 9091,
-        journalFilename: journalFilename,
+        journalFilename: config.journalFilename
+            || config.logDir && config.journalName && (config.logDir + '/' + config.journalName)
+            || null,
         labels: config.labels || {},
     };
     return options;
 }
 
-function sendTo( target, name, message ) {
-    if (target && target.send) {
-        try {
-            target.send({ n: name, m: message });
-        } catch (err) {
-        }
-    }
+PushGateway.prototype = PushGateway.prototype;
+
+
+const scriptPath = require.resolve(__filename);
+if (process.argv[1] === scriptPath || process.argv[1] === path.dirname(scriptPath)) {
+    // if loaded directly, start the service
+    pushGateway.runServer();
+}
+else {
+    // if loaded as part of another script, just export the singleton
 }
