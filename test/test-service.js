638e8ae29860092835224f2ae7f2bae4353f64c7
Andras <andras@kinvey.com>
2018-02-24 21:40:02 -0500

    start test-service

--- test/test-service.js
+++ test/test-service.js
@@ -1,127 +1,67 @@
 'use strict';
 
-const Path = require('path');
+const net = require('net');
 const app = require('../lib/app');
+const serv = require('../lib/service');
 
 module.exports = {
-    afterEach: function(done) {
-        unrequire('../lib/service.js');
-        process.removeAllListeners('message');
-        process.removeAllListeners('disconnect');
-        done();
+    'should export expected functions': function(t) {
+        t.equal(typeof serv.createServer, 'function');
+        t.equal(typeof serv.forkServer, 'function');
+        t.done();
     },
 
-    'script should start the service and listen': function(t) {
-        var message;
-        const spySend = t.stubOnce(process, 'send', function(m) { message = m });
-        const spyCreate = t.stubOnce(app, 'createServer', function(config, cb) { return cb(null, { port: 13337, pid: 123456 }) });
+    'createServer': {
+        'should listen on configured port': function(t) {
+            const server = serv.createServer({ port: 13337 }, function(err, info) {
+                t.ifError(err);
+                t.equal(info.pid, process.pid);
+                t.equal(info.port, 13337);
+                // connect must not throw
+                const sock = new net.Socket();
+                sock.connect(13337, function() {
+                    sock.end();
+                    server.close(function(){
+                        t.done();
+                    });
+                })
+            })
+        },
 
-        unrequire('../lib/service-worker.js');
-        require('../lib/service-worker.js');
-        process.emit('message', { n: 'createServer', m: { port: 13337 } });
-
-        setTimeout(function() {
-            t.ok(spySend.called);
-            t.ok(spyCreate.called);
-            t.deepEqual(message, { n: 'ready', m: { pid: 123456, port: 13337 } });
-            t.deepEqual(Object.keys(message.m), ['pid', 'port']);
+        'should createServer without a callback': function(t) {
+            const spy = t.stubOnce(app, 'createServer', function(opts, cb) { cb(null, { port: opts.port, pid: 12345 }) });
+            const server = serv.createServer({ port: 13338 });
+            t.ok(spy.called);
+            t.contains(spy.args[0][0], { port: 13338, labels: {} });
             t.done();
-        }, 20);
-    },
+        },
 
-    'script should ignore irrelevant message': function(t) {
-        var message;
-        const spySend = t.stub(process, 'send', function(m) { message = m });
-        const spyCreate = t.stubOnce(app, 'createServer', function(config, cb) { return cb(null, { port: 13337, pid: 123456 }) });
-
-        unrequire('../lib/service-worker.js');
-        require('../lib/service-worker.js');
-        process.emit('message');
-        process.emit('message', {});
-        process.emit('message', { n: 'some other message' });
-
-        setTimeout(function() {
-            spySend.restore();
-            spyCreate.restore();
-            t.ok(!spySend.called);
-            t.ok(!spyCreate.called);
+        'should assemble and pass journalFilename': function(t) {
+            const spy = t.stub(app, 'createServer', function(){});
+            serv.createServer({ port: 123, journalFilename: '/logs/test-metrics.jrn' });
+            serv.createServer({ port: 1234, logDir: '.', journalName: 'test-metrics.jrn' });
+            t.ok(spy.called);
+            spy.restore();
+            t.contains(spy.args[0][0], { port: 123, journalFilename: '/logs/test-metrics.jrn' });
+            t.contains(spy.args[1][0], { port: 1234, journalFilename: './test-metrics.jrn' });
             t.done();
-        }, 20);
+        },
+
+        'verbose mode should write startup message': function(t) {
+            var output = "";
+            t.stubOnce(app, 'createServer', function(opts, cb) { cb(null, { port: opts.port, pid: 12345 }) });
+            const spy = t.stub(process.stdout, 'write', function(chunk) { output += chunk });
+            const server = serv.createServer({ port: 9091, verbose: true }, function(err, info) {
+                spy.restore();
+                t.ifError(err);
+                t.contains(output, 'Starting');
+                t.contains(output, 'Listening');
+                t.done();
+            })
+        },
+       
     },
 
-    'script should return createServer error': function(t) {
-        var message;
-        const error = new Error('createServer error');
-        const spySend = t.stubOnce(process, 'send', function(m) { message = m });
-        const spyCreate = t.stubOnce(app, 'createServer', function(config, cb) { return cb(error) });
-
-        unrequire('../lib/service-worker.js');
-        require('../lib/service-worker.js');
-        process.emit('message', { n: 'createServer', m: { port: 13337 } });
-
-        setTimeout(function() {
-            t.deepEqual(message, { n: 'error', m: { message: error.message, stack: error.stack } });
-            t.done();
-        }, 20);
+    'forkServer': {
     },
-
-    'script should return error if createServer throws': function(t) {
-        var message;
-        const error = new Error('createServer exception');
-        const spySend = t.stubOnce(process, 'send', function(m) { message = m });
-        const spyCreate = t.stubOnce(app, 'createServer', function(config, cb) { throw error });
-
-        unrequire('../lib/service-worker.js');
-        require('../lib/service-worker.js');
-        process.emit('message', { n: 'createServer', m: { port: 13337 } });
-
-        setTimeout(function() {
-            t.deepEqual(message, { n: 'error', m: { message: error.message, stack: error.stack } });
-            t.done();
-        }, 20);
-    },
-
-    'script should kill self on disconnect': function(t) {
-        const spyKill = t.stubOnce(process, 'kill');
-        const spyCreate = t.stubOnce(app, 'createServer', function(config, cb) { return cb(null, { port: 13337, pid: 123456 }) });
-
-        unrequire('../lib/service-worker.js');
-        require('../lib/service-worker.js');
-
-        process.emit('message', { n: 'createServer', m: { timeout: 1 } });
-        process.emit('disconnect');
-        setTimeout(function() {
-            t.ok(spyKill.called);
-            t.done();
-        }, 5);
-    },
-}
-
-// from qmock:
-function unrequire( moduleName ) {
-    var pathname;
-
-    // require.resolve throws with "cannot find module", work around with path.resolve
-    if (moduleName[0] === '.') pathname = Path.resolve(__dirname + '/' + moduleName);
-    else if (moduleName[0] === '/') pathname = Path.resolve(moduleName);
-    else pathname = require.resolve(moduleName);
-
-    var ix, mod = require.cache[pathname];
-    delete require.cache[pathname];
-
-    while (module.parent) module = module.parent;
-    unlinkAll(module.children, mod);
-
-    function unlinkAll( children, mod ) {
-        var ix;
-        while ((ix = children.indexOf(mod)) >= 0) {
-            children.splice(ix, 1);
-        }
-        if (children._qmock_visited) return;
-        children._qmock_visited = true;
-        for (var i=0; i<children.length; i++) {
-            unlinkAll(children[i].children, mod);
-        }
-        delete children._qmock_visited;
-    }
 }
